882484598cef15da4efaf1cb6ba384eb87af2d9e
GuilhermeFonseca10 <guilhermeelviro2017@gmail.com>
2023-02-28 11:25:13 -0300

    create models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from recipes.models import Category
+from recipes.models import Recipe
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    admin.site.register(Category)
+
+class RecipeAdmin(admin.ModelAdmin):
+    admin.site.register(Recipe)
