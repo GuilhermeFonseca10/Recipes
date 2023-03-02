3312106958db9cb67050bb3245f3a6d722467e7c
GuilhermeFonseca10 <guilhermeelviro2017@gmail.com>
2023-03-02 20:38:41 -0300

    atualizacoes

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,16 +1,17 @@
-from django.urls import path
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
 from . import views
 
-app_name = 'recipes'
+app_name = "recipes"
 
 urlpatterns = [
-    
-    path('', views.home, name="home"),
-    path('recipes/category/<int:category_id>/', views.category, name="category"),
-    path('recipes/<int:id>/', views.recipe, name="recipe"),
-     
+    path("", views.home, name="home"),
+    path(
+        "recipes/category/<int:category_id>/", views.category, name="category"
+    ),
+    path("recipes/<int:id>/", views.recipe, name="recipe"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
